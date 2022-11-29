d841ddf8248fbcf4218dd9ddbf4987fc438cfda3
Tomás Salina <salinatomass53@gmail.com>
2022-11-29 18:34:17 -0300

    Purge CSS

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js}'],
+  content: ['./index.html', './src/**/*.{html,js}'],
   darkMode: 'class',
   theme: {
     extend: {
