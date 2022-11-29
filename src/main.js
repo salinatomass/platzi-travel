e03f6490ba376674ec7fe0dfbe5fcf2dd0527fd4
Tomás Salina <salinatomass53@gmail.com>
2022-11-29 18:31:20 -0300

    Dark mode

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 import './css/main.css'
+
+const $html = document.getElementById('html')
+const $buttonsDark = document.querySelectorAll('.toggleDarkButton')
+
+const toggleDarkMode = () => {
+  $html.classList.toggle('dark')
+}
+
+$buttonsDark.forEach(btn => btn.addEventListener('click', toggleDarkMode))
